ec2c478ca0e487dbff198276387c045b6efb759e
Christos Mantas <the1pro@gmail.com>
2022-11-04 15:22:21 +0000

    W2, L1: Implement `--min_products`

--- week2/createContentTrainingData.py
+++ week2/createContentTrainingData.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 import os
 import xml.etree.ElementTree as ET
 from pathlib import Path
+from itertools import chain
+from collections import defaultdict, Counter
 
 def transform_name(product_name):
     # IMPLEMENT
@@ -18,6 +20,7 @@ general = parser.add_argument_group("general")
 general.add_argument("--input", default=directory,  help="The directory containing product data")
 general.add_argument("--output", default="/workspace/datasets/fasttext/output.fasttext", help="the file to output to")
 general.add_argument("--label", default="id", help="id is default and needed for downsteam use, but name is helpful for debugging")
+general.add_argument("--limit-files", default=None, type=int)
 
 # IMPLEMENT: Setting min_products removes infrequent categories and makes the classifier's task easier.
 general.add_argument("--min_products", default=0, type=int, help="The minimum number of products per category (default is 0).")
@@ -58,12 +61,31 @@ def _label_filename(filename):
               labels.append((cat, transform_name(name)))
     return labels
 
+def remove_low_freq_categories(data, min_count=1):
+    # create a category counts dict from all data
+    cat_counts = defaultdict(int)
+    for cat, _ in data:
+        cat_counts[cat] += 1
+    # keep elements whose categories that have counts > min_count
+    return filter(lambda t: cat_counts[t[0]]>=min_count, data)
+
+
 if __name__ == '__main__':
     files = glob.glob(f'{directory}/*.xml')
+    #Limit files to process (for testing purposes)
+    if args.limit_files:
+        files = files[:args.limit_files]
+
     print("Writing results to %s" % output_file)
     with multiprocessing.Pool() as p:
+        # read data from each xml to a list using a pool of workers 
+        # (creates an iterable of lists of label-text pairs)
         all_labels = tqdm(p.imap(_label_filename, files), total=len(files))
+        # flatten data from all files (creates a list of label-text pairs)
+        data_lines = list(chain(*all_labels))
+        # remove low frequency cateogories
+        data_lines = remove_low_freq_categories(data_lines, min_products)
+        # write to output file
         with open(output_file, 'w') as output:
-            for label_list in all_labels:
-                for (cat, name) in label_list:
-                    output.write(f'__label__{cat} {name}\n')
+            for (cat, name) in data_lines:
+                output.write(f'__label__{cat} {name}\n')
